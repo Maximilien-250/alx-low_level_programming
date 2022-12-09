0f127c0501652fd02bac60b9ef2d387fb0f54539
Maximilien-250 <ntwarrifist2009@gmail.com>
2022-12-08 17:47:46 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry Point
- * return: Always 0 (Success)
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
-	puts("\"Programming is like develop a multlingual puzzel");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
